957316478e85d82474d232bc0903f7b707cec9a3
shazwanr <shazwanr@gmail.com>
2022-09-30 11:23:03 +0800

    changed to use object id instead of array index to setState

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -17,10 +17,10 @@ function User() {
             });
     }, []);
 
-    const toggleFavorite = (index: number) => {
+    const toggleFavorite = (id: number) => {
         setUsers(
-            users.map((data: any, dataIndex: number) => {
-                if (dataIndex === index) {
+            users.map((data: any) => {
+                if (data.id === id) {
                     return { ...data, favorite: !data.favorite }
                 }
                 else {
@@ -30,15 +30,15 @@ function User() {
         )
     }
 
-    const removeUser = (index: number) => {
+    const removeUser = (id: number) => {
         setUsers(
-            users.filter((data: any, dataIndex: number) => dataIndex !== index)
+            users.filter((data: any) => data.id !== id)
         )
     }
 
     return (
         <div className='users'>
-            {users && users.map((user: any, index: number) => (
+            {users && users.map((user: any) => (
                 <div className="card">
                     <img className='user-image' src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}></img>
                     <div className='card-body'>
@@ -48,9 +48,9 @@ function User() {
                         <div className='user-detail'><FaGlobe className='card-icon' />{user.website}</div>
                     </div>
                     <div className='card-footer'>
-                        <FaRegHeart className='card-button' onClick={() => toggleFavorite(index)} style={{ color: user.favorite ? '#ea3427' : '#525152' }} />|
+                        <FaRegHeart className='card-button' onClick={() => toggleFavorite(user.id)} style={{ color: user.favorite ? '#ea3427' : '#525152' }} />|
                         <FaEdit className='card-button' />|
-                        <FaTrash className='card-button' onClick={() => removeUser(index)} />
+                        <FaTrash className='card-button' onClick={() => removeUser(user.id)} />
                     </div>
                 </div>
             ))}
